f73c212746a8c465307cce2c0c7d8b29089b232a
Jason CheongKeeYou <jasonc@nulogy.com>
2018-08-01 15:37:40 -0400

    Parse multiple months

--- bgg.rb
+++ bgg.rb
@@ -3,26 +3,37 @@ require 'net/http'
 require 'nokogiri'
 require 'ostruct'
 require 'uri'
+require 'active_support/all'
 
 class Bgg
   def run
-    url = url_for_month("2018-07-01")
-    file = open_url(url)
-    doc = Nokogiri::HTML(file)
+    @games = {}
+    @months = []
+    month = (Date.today - 3.months).beginning_of_month
+    while month < Date.today.beginning_of_month
+      @months << month.to_s
+      url = url_for_month(month)
+      file = open_url(url)
+      doc = Nokogiri::HTML(file)
 
-    @games = doc.css('.forum_table')[1].css('tr')[1..-1].map.with_index do |row, rank|
-      link, _, plays = row.css('td')
-      anchor = link.css('a')
-      href = anchor[0]['href']
-      name = anchor[0].content
-      OpenStruct.new(href: href, name: name, rank: rank + 1)
+      doc.css('.forum_table')[1].css('tr')[1..-1].map.with_index do |row, rank|
+        link, _, plays = row.css('td')
+        anchor = link.css('a')
+        href = anchor[0]['href']
+        name = anchor[0].content
+
+        @games[name] ||= OpenStruct.new(href: href, name: name, ranks: {})
+
+        @games[name].ranks[month.to_s] = rank + 1
+      end
+      month += 1.month
     end
 
     File.write('bgg.html', ERB.new(File.read('bgg.erb')).result(binding))
   end
 
   def url_for_month(month)
-    "https://boardgamegeek.com/plays/bygame/start/2018-07-01/end/2018-07-31?sortby=distinctusers"
+    "https://boardgamegeek.com/plays/bygame/start/#{month.beginning_of_month}/end/#{month.end_of_month}?sortby=distinctusers"
   end
 
   def open_url(url)
