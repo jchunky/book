813d7c7d96ca4e3edda5cc62352527821309bf72
Jason CheongKeeYou <jasonc@nulogy.com>
2018-08-01 22:45:26 -0400

    Refactor

--- bgg.rb
+++ bgg.rb
@@ -1,15 +1,20 @@
+require 'active_support/all'
 require 'erb'
 require 'net/http'
 require 'nokogiri'
 require 'ostruct'
 require 'uri'
-require 'active_support/all'
 
 class Bgg
+  NUMBER_OF_MONTHS = 72
+
   def run
     @months = months
     @games = @months
-      .flat_map { |month| games_for_month(month) }
+      .map { |month| [month, url_for_month(month)] }
+      .map { |month, url| [month, read_url(url)] }
+      .map { |month, file| [month, Nokogiri::HTML(file)] }
+      .flat_map { |month, doc| games_for_doc(month, doc) }
       .reduce({}) do |memo, game|
         memo[game.name] ||= game
         memo[game.name].ranks.merge!(game.ranks)
@@ -21,7 +26,7 @@ class Bgg
 
   def months
     result = []
-    month = (Date.today - 72.months).beginning_of_month
+    month = (Date.today - NUMBER_OF_MONTHS.months).beginning_of_month
     while month < Date.today.beginning_of_month
       result << month
       month += 1.month
@@ -29,27 +34,11 @@ class Bgg
     result
   end
 
-  def games_for_month(month)
-    url = url_for_month(month)
-    file = open_url(url)
-    doc = Nokogiri::HTML(file)
-    doc.css('.forum_table')[1].css('tr')[1..-2].map.with_index do |row, rank|
-      link, _, plays = row.css('td')
-      anchor = link.css('a')
-      href = anchor[0]['href']
-      name = anchor[0].content
-
-      game = OpenStruct.new(href: href, name: name, ranks: {})
-      game.ranks[month.to_s] = rank + 1
-      game
-    end
-  end
-
   def url_for_month(month)
     "https://boardgamegeek.com/plays/bygame/start/#{month.beginning_of_month}/end/#{month.end_of_month}?sortby=distinctusers"
   end
 
-  def open_url(url)
+  def read_url(url)
     cache(url) { open(url) }
   end
 
@@ -61,6 +50,19 @@ class Bgg
 
   def open(url)
     Net::HTTP.get(URI.parse(url))
+  end
+
+  def games_for_doc(month, doc)
+    doc.css('.forum_table')[1].css('tr')[1..-2].map.with_index do |row, rank|
+      link, _, plays = row.css('td')
+      anchor = link.css('a')
+      href = anchor[0]['href']
+      name = anchor[0].content
+
+      game = OpenStruct.new(href: href, name: name, ranks: {})
+      game.ranks[month.to_s] = rank + 1
+      game
+    end
   end
 
   def write_output
