3745815c627437d33eb33bd6fd5c418a1b7437d4
Jason CheongKeeYou <jasonc@nulogy.com>
2018-10-23 11:30:14 -0400

    Display ios

--- top_ranked.rb
+++ top_ranked.rb
@@ -37,13 +37,14 @@ class TopRanked
 
   def games_for_doc(doc)
     doc.css('.collection_table')[0].css('tr')[1..-1].map.with_index do |row, rank|
-      rank, _, title, _, rating, voters = row.css('td')
+      rank, _, title, _, rating, voters, *_, shop = row.css('td')
 
       rank = rank.css('a')[0]['name'] rescue nil
       href = title.css('a')[0]['href']
       name = title.css('a')[0].content
       rating = rating.content
       voters = voters.content
+      ios = shop.to_s.include?("iOS App:")
 
       OpenStruct.new(
         href: href,
@@ -51,7 +52,8 @@ class TopRanked
         rank: rank,
         rating: rating,
         voters: voters,
-        key: Utils.generate_key(name)
+        key: Utils.generate_key(name),
+        ios: ios
       )
     end.compact
   end
