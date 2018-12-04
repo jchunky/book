f39b6ab9dc50949d91089e15694dccebfac8fe80
Jason Cheong-Kee-You <jasonc@nulogy.com>
2018-12-04 10:59:21 -0500

    Display ranked games as well as S&L games

--- lib/bgg.rb
+++ lib/bgg.rb
@@ -6,9 +6,10 @@ class Bgg
   def run
     @months = months_display
 
-    @games = Snake.new.games
-      .tap { |games| merge_games(games, top_played) }
-      .tap { |games| merge_games(games, top_ranked) }
+    @games = snake
+      .merge(top_played) { |key, game1, game2| merge_ostructs(game1, game2) }
+      .merge(top_ranked) { |key, game1, game2| merge_ostructs(game1, game2) }
+      .values
       .select(&method(:display_game?))
       .sort_by(&method(:rank))
 
@@ -16,12 +17,12 @@ class Bgg
   end
 
   def display_game?(game)
-    return true if game.ts_added > "2018-11-22"
+    return true if game.ts_added && game.ts_added > "2018-11-22"
     return false if game.ts_added && !game.players
     return false if game.year && game.year.to_i < 1980
     return false if game.category == "Dexterity"
     return false if game.category == "Nostalgia"
-    return false if game.players && game.player_count < 300
+    return false if game.player_count.to_i < 300
     true
   end
 
@@ -45,6 +46,10 @@ class Bgg
     File.write('output/bgg.html', html)
   end
 
+  def snake
+    @snake ||= Snake.new.games.map { |g| [g.key, g] }.to_h
+  end
+
   def top_played
     @top_played ||= TopPlayed.new.games.map { |g| [g.key, g] }.to_h
   end
@@ -53,12 +58,8 @@ class Bgg
     @top_ranked ||= TopRanked.new.games.map { |g| [g.key, g] }.to_h
   end
 
-  def merge_games(games1, games2)
-    games1.each { |g| merge_ostructs(g, games2[g.key]) }
-  end
-
   def merge_ostructs(ostruct1, ostruct2)
-    ostruct2.to_h.each { |k, v| ostruct1[k] = v }
+    OpenStruct.new(ostruct1.to_h.merge(ostruct2.to_h))
   end
 end
 
