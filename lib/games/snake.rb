c17c2107672d7eec7e687e31a85461c89f2166d5
Jason Cheong-Kee-You <jasonc@nulogy.com>
2019-02-18 22:42:11 -0500

    Highlight small box games

--- lib/games/snake.rb
+++ lib/games/snake.rb
@@ -1,6 +1,7 @@
 class Snake
   CATEGORIES = ["Nostalgia", "Dexterity", "Children's", "Cooperative", "Party", "Light Strategy", "Strategy", "Word", "Abstract", "Trivia", "Greatest Hits"]
   FILES = %w[abstract childrens cooperative dexterity greatest_hits light_strategy new_arrivals nostalgia party strategy trivia word].map { |f| "input/#{f}.json" }
+  SMALL_BOX_LOCATIONS = %w[01C 02C 06C 07D 10C 11A 13C 15C 20B 20C 20D 21B 21E 22D 29B 29C 29D]
 
   def games
     FILES
@@ -26,13 +27,18 @@ class Snake
       ts_added: Time.at(data['ts_added'].to_i).strftime("%Y-%m-%d"),
       sell_product: data['sell_product'],
       employees_teachable: (data['employees_teachable'].size rescue 0),
-      category: category(data)
+      category: category(data),
+      small_box: small_box(data)
     )
   end
 
   def location(data)
     location = data['shelf_location']
     location.size == 2 ? "0" + location : location
+  end
+
+  def small_box(data)
+    SMALL_BOX_LOCATIONS.include?(location(data))
   end
 
   def category(data)
