dc72c3bf3a43a225c83606cb2b1d4f57536fe320
Chunky <jasonc@nulogy.com>
2020-08-25 09:20:40 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -16,8 +16,8 @@ class Bgg
     return false if game.rank < 1
     return false if game.play_rank < 1
 
-    # return false unless game.was_in_top_100_for_6_years?
-    return false unless game.was_in_top_100?
+    return false unless game.was_in_top_100_for_6_years?
+    # return false unless game.was_in_top_100?
     # return false if game.recent?
     # return false if game.play_rank > PLAY_RANK_THRESHOLD
     # return false if game.trend == :down
