e03c54b2b8cf2a0355c4011de164e093cc3e4312
Chunky <jasonc@nulogy.com>
2020-10-07 00:29:21 -0400

    Display plays stats by month

--- bgg.rb
+++ bgg.rb
@@ -7,8 +7,8 @@ require "uri"
 Dir["lib/*.rb"].each { |f| require_relative f }
 
 class Bgg
-  # BY_MONTH = true
-  BY_MONTH = false
+  BY_MONTH = true
+  # BY_MONTH = false
   PLAY_RANK_THRESHOLD = 100
   YEARS_OLD = 4
   MAX_GAME_YEAR = TopPlayed.last_year.year - YEARS_OLD
