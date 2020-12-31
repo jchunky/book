22d37443667957e4d1389e37e5b3bb83a79cea61
Chunky <jasonc@nulogy.com>
2020-12-31 15:36:39 -0500

    Download all children type books

--- lib/game.rb
+++
@@ -1,2 +0,0 @@
-Game = Struct.new(:title, :copies, keyword_init: true) do
-end
--- lib/top_ranked.rb
+++ lib/top_ranked.rb
@@ -1,28 +1,39 @@
 class TopRanked
+  BookType = Struct.new(:name, :id)
+  Book = Struct.new(:title, :copies, :book_type)
+
+  BOOK_TYPES = [
+    PIC = BookType.new("PIC", "38773&Ne=38769"),
+    BR = BookType.new("BR", "38771&Ne=38769"),
+    ER = BookType.new("ER", "38772&Ne=38769"),
+    APIC = BookType.new("APIC", "38770&Ne=38769"),
+    FICTION = BookType.new("FICTION", "38790+37846&Ne=27605"),
+  ]
+
   def games
-    (1..10)
-      .lazy
-      .map { |page| url_for_page(page) }
-      .map { |url| Utils.read_url(url) }
-      .map { |file| Nokogiri::HTML(file) }
-      .flat_map(&method(:games_for_doc))
-      .force
+    BOOK_TYPES.flat_map do |book_type|
+      (1..10)
+        .lazy
+        .map { |page| url_for_page(book_type, page) }
+        .map { |url| Utils.read_url(url) }
+        .map { |file| Nokogiri::HTML(file) }
+        .flat_map { |doc| games_for_doc(book_type, doc) }
+        .force
+    end
   end
 
-  def url_for_page(page)
+  def url_for_page(book_type, page)
     index = (page - 1) * 150
-    "https://www.torontopubliclibrary.ca/search.jsp?Erp=150&N=38773+20206+37751+37918&Ne=38769&No=#{index}&Ns=p_date_acquired_sort&Nso=1&Ntk=Keyword_Anywhere&view=grid"
+
+    "https://www.torontopubliclibrary.ca/search.jsp?Erp=150&N=20206+37751+37918+#{book_type.id}&No=#{index}&Ns=p_date_acquired_sort&Nso=1&Ntk=Keyword_Anywhere&view=grid"
   end
 
-  def games_for_doc(doc)
+  def games_for_doc(book_type, doc)
     doc.css(".search-results-column > .row").last.css(".details").map do |row|
       title = row.css(".ellipsis_text").first.content
       copies = row.css(".p").first.content.scan(/\d+ copies/).first.to_i
 
-      Game.new(
-        title: title,
-        copies: copies
-      )
+      Book.new(title, copies, book_type.name)
     end
   end
 end
