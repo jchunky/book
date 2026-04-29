8f8d922cb64c130aaede8fa7882bc4e195321d67
Jason CheongKeeYou <jasonc@nulogy.com>
2026-04-28 23:40:53 -0400

    Update filter criteria

--- lib/services/movie_filter.rb
+++ lib/services/movie_filter.rb
@@ -5,11 +5,20 @@ module Services
     def self.keep?(item) = new(item).keep?
 
     def keep?
-      # return false if animation? || documentary? || horror? || musical? || juvenile? || restricted?
+      # return false if animation? || documentary? || horror? || musical? || juvenile?
       # return false unless box_office.to_i >= 10
       # return false unless certified_fresh?
+      # return false unless display_year.to_i >= 1985
       # return false unless rated?
 
+      # == KIDS ==
+      # return false if restricted?
+
+      # == ME ==
+      # return false unless must_see?
+      # return false unless display_year.to_i >= 2015
+
+      # == UNPROCESSED ==
       return false if processed?
       return false unless must_see? || certified_fresh?
 
