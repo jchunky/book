506b9c915d0d5187bab9fbf33fe15131d893d751
Jason CheongKeeYou <jasonc@nulogy.com>
2026-05-04 08:20:05 -0400

    Update filter criteria

--- lib/services/movie_filter.rb
+++ lib/services/movie_filter.rb
@@ -6,27 +6,37 @@ module Services
 
     def keep?
       # == KIDS ==
-      # return false unless rated?
-      # return false unless box_office.to_i >= 20
-      # return false unless certified_fresh?
-      # return false if animation? || documentary? || horror? || musical? || juvenile?
-      # return false unless display_year.to_i >= 2000
+      # return false unless meet_minimum_quality_bar?
+      # return false unless meet_personal_interest_criteria?
       # return false if restricted?
       # return false unless box_office.to_i >= 50
 
       # == DATE NIGHT ==
-      # return false unless rated?
-      # return false unless box_office.to_i >= 20
-      # return false unless certified_fresh?
-      # return false if animation? || documentary? || horror? || musical? || juvenile?
-      # return false unless display_year.to_i >= 2000
+      # return false unless meet_minimum_quality_bar?
+      # return false unless meet_personal_interest_criteria?
       # return false unless metacritic.to_i >= 70
 
       # == UNPROCESSED ==
-      return false unless rated?
-      return false unless box_office.to_i >= 20
+      return false unless meet_minimum_quality_bar?
       return false if processed?
       return false unless must_see? || certified_fresh?
+
+      true
+    end
+
+    private
+
+    def meet_minimum_quality_bar?
+      return false unless rated?
+      return false unless box_office.to_i >= 20
+      return false unless certified_fresh?
+
+      true
+    end
+
+    def meet_personal_interest_criteria?
+      return false if animation? || documentary? || horror? || musical? || juvenile?
+      return false unless display_year.to_i >= 2000
 
       true
     end
