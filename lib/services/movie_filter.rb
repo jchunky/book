5d06010c2b95730a52147ebf7957089bafb84907
Jason CheongKeeYou <jasonc@nulogy.com>
2026-05-23 08:08:05 -0400

    Update filter criteria

--- lib/services/movie_filter.rb
+++ lib/services/movie_filter.rb
@@ -5,45 +5,17 @@ module Services
     def self.keep?(item) = new(item).keep?
 
     def keep?
-      # == KIDS ==
-      return false unless meet_minimum_quality_bar?
-      return false unless meet_personal_interest_criteria?
-      # return false unless box_office.to_i >= 50
-      # return false if !restricted?
+      return false if animation? || musical? || juvenile?
+      # return false if restricted?
+      # return false if processed?
+      # return false unless foreign?
       return false unless rated?
 
-      # == FOREIGN ==
-      # return false unless foreign?
-      # return false unless popularity.to_i >= 1
-      # return false unless metacritic.to_i >= 70
-      # return false unless rotten_tomatoes.to_i >= 75
-      # return false unless box_office.to_i >= 1
-
-      # == DATE NIGHT ==
-      # return false unless meet_minimum_quality_bar?
-      # return false unless meet_personal_interest_criteria?
-      # return false unless metacritic.to_i >= 70
-
-      # == UNPROCESSED ==
       return false unless box_office.to_i >= 1
-      # return false if processed?
-      # return false unless must_see? || certified_fresh?
-
-      true
-    end
-
-    private
-
-    def meet_minimum_quality_bar?
-      # return false unless box_office.to_i.between?(10, 19)
-      return false unless certified_fresh?
-
-      true
-    end
-
-    def meet_personal_interest_criteria?
-      return false if animation? || musical? || juvenile?
       # return false unless display_year.to_i >= 2000
+      # return false unless metacritic.to_i >= 70
+      # return false unless popularity.to_i >= 1
+      # return false unless rotten_tomatoes.to_i >= 75
 
       true
     end
