1c7eec55f62fe68397739b405c4ed1b73435d95c
Jason CheongKeeYou <jasonc@nulogy.com>
2026-05-27 07:46:29 -0400

    Update list of watched movies

--- lib/services/movie_filter.rb
+++ lib/services/movie_filter.rb
@@ -7,20 +7,20 @@ module Services
     def keep?
       return false if animation? || musical? || juvenile?
 
-      return false unless omdb_matched?
+      # return false unless omdb_matched?
       # return false unless omdb_unmatched?
       # return false unless omdb_unqueried?
 
-      # return false unless omdb.movie?
-      # return false unless rated?
-      # return false unless box_office.to_i >= 1
-      # return false unless metacritic.to_i >= 1
-      # return false unless rotten_tomatoes.to_i >= 1
+      return false unless omdb.movie?
+      return false unless rated?
+      return false unless box_office.to_i >= 1
+      return false unless metacritic.to_i >= 1
+      return false unless rotten_tomatoes.to_i >= 1
 
       # return false unless watched?
       # return false unless loved?
       # return false if restricted?
-      # return false if processed?
+      return false if processed?
       # return false unless foreign?
 
       # return false unless display_year.to_i >= 2000
